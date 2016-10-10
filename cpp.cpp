0aaf0ece8767212fda68100302c860eda2ac93e1
Dmitry Ponyatov <dponyatov@gmail.com>
2016-10-10 18:17:15 +0400

    + pcb/layers pcb/pads
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -1,9 +1,9 @@
 #include "hpp.hpp"
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
-int main() {
+int main() { 
 	// .neutral parse
-	yyparse();
+	pcb_init(); yyparse();
 	// .dat
 	cout << "\n\n=================\n\n";
 	for (auto it=pcb.begin(),e=pcb.end();it!=e;it++)
@@ -14,12 +14,18 @@ int main() {
 	dsn << "\t(resolution " << pcb["B_UNITS"]->val << " 100)\n";// B_UNITS
 															// pcb structure
 	dsn << "\t(structure\n";								
-	dsn << "\t\t(boundary (path pcb 0.5 ";					// outline polygon
+	// \ outline polygon
+	dsn << "\t\t(boundary (path pcb 0.5 ";					
 	Sym* BE = pcb["BOARD_EDGE_OUTLINE"];
 	for (int i=0;i<BE->size();i++)
 		dsn << BE->nest[i]->nest[0]->val<<" "<<BE->nest[i]->nest[1]->val<<" ";
 	dsn << "))\n";
-	dsn << "\t\t(layer bot (type signal))\n";
+	// /
+	// \ layers
+	Sym* LE = pcb["layer"];
+	for (auto it=LE->lookup.begin(),e=LE->lookup.end();it!=e;it++)
+		dsn << "\t\t(layer " << it->first << " (type signal))\n";
+	// /
 	dsn << "\t)\n";
 	dsn << ")\n";
 }
@@ -30,16 +36,22 @@ void Sym::push(Sym*o) { nest.push_back(o); }
 string Sym::head() { return val; }
 string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
 string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	for (auto it=lookup.begin(),e=lookup.end();it!=e;it++)
+		S += "\n"+pad(depth+1)+it->first+"="+it->second->dump(depth+2);
 	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
 		S += (*it)->dump(depth+1);
 	return S; }
 
 int Sym::size() { return nest.size(); }
 
-map<string,Sym*> pcb;
-
 Str::Str(string V):Sym(V){}
 string Str::head() { return "'"+val+"'"; }
 
 List::List():Sym("[]"){}
 
+map<string,Sym*> pcb;
+
+void pcb_init() {
+	pcb["pad"]=new Sym("pad");
+	pcb["layer"]=new Sym("layer");
+}
--- hpp.hpp
+++ hpp.hpp
@@ -12,12 +12,14 @@ struct Sym {
 	string val;
 	Sym(string);
 	vector<Sym*> nest; void push(Sym*);
+	map<string,Sym*> lookup;
 	virtual string head(); string pad(int);
 	virtual string dump(int=0);
 	virtual int size();
 };
 
 extern map<string,Sym*> pcb;
+extern void pcb_init();
 
 struct Str:Sym { Str(string); string head(); };
 
